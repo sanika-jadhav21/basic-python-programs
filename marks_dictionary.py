9700ace20df7cb9422a4c0b1c9f1ddaeb7394a2b
sanika_jadhav <jadhavsanika560@gmail.com>
2024-11-10 01:21:47 +0530

    Create marks_dictionary.py

---
+++ marks_dictionary.py
@@ -0,0 +1,14 @@
+# Inserting subjects marks of student 
+marks={}
+
+a=int(input("Enter 1st sub marks:"))
+marks.update({"maths":a})
+print(marks)
+
+b=int(input("Enter 2nd sub marks:"))
+marks.update({"phy":b})
+print(marks)
+
+c=int(input("Enter 3rd sub marks:"))
+marks.update({"chem":c})
+print(marks)
