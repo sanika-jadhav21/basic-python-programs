4d309978a53a51bd396acf0388b2a925dcb119a3
sanika_jadhav <jadhavsanika560@gmail.com>
2024-11-10 01:46:53 +0530

    print_list_in_single_line.py

--- print_list_in_single_line.py
+++ print_list_in_single_line.py
@@ -2,8 +2,10 @@
 
 num=[1,2,3,45,6,7,8,9]
 
-def print_list(num):
-    print(num)
+def print_list():
+    for i in num:
+        print(i,end=" ")
+    
     return num
-
-print_list(num)
+    
+print_list()  
