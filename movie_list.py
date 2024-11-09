a5bc1050ee31938a58f26c8a32337c72d86170bc
sanika_jadhav <jadhavsanika560@gmail.com>
2024-11-10 01:12:06 +0530

    Create movie_list.py

---
+++ movie_list.py
@@ -0,0 +1,8 @@
+#input 3 movies list and store in list
+
+movie=[]
+movie.append(input("Enter 1st movie"))
+movie.append(input("Enter 2nd movie"))
+movie.append(input("Enter 3rd movie"))
+
+print(movie)
