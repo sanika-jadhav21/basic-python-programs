d2078e29f443c040c7914f28a35708a8cf89c403
sanika_jadhav <jadhavsanika560@gmail.com>
2024-11-10 02:31:14 +0530

    Fibonacci _Series.py

---
+++ Fibonacci _Series.py
@@ -0,0 +1,13 @@
+# Fibonacci Series 
+"""
+n = int(input("Enter the number: "))
+print("Fibonacci Sequence:")
+
+def fibonacci_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+for i in range(n):
+    print(fibonacci_recursive(i))
